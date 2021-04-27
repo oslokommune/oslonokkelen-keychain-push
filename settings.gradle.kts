122f38d372ad29cedd7529760ca0b4848403cbe9
Kim A. Betti <kim.betti@gmail.com>
2021-04-27 15:43:51 +0200

    Docs - Event callbacks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,6 @@
 rootProject.name = "oslonokkelen-keychain-push"
 
 include(":oslonokkelen-keychain-push-protobuf")
+include(":oslonokkelen-keychain-push-client")
+include(":oslonokkelen-keychain-push-client-ktor")
 include(":oslonokkelen-keychain-push-docs")
