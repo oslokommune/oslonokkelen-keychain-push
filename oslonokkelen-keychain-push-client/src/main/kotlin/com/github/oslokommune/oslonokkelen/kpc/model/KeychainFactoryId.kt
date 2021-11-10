ca2a676b3838f766394245d8d5b8cb6c3de066c4
Kim A. Betti <kim.betti@gmail.com>
2021-11-10 15:14:32 +0100

    Formatting

--- oslonokkelen-keychain-push-client/src/main/kotlin/com/github/oslokommune/oslonokkelen/kpc/model/KeychainFactoryId.kt
+++ oslonokkelen-keychain-push-client/src/main/kotlin/com/github/oslokommune/oslonokkelen/kpc/model/KeychainFactoryId.kt
@@ -4,8 +4,6 @@ package com.github.oslokommune.oslonokkelen.kpc.model
  * Identifies a keychain factory. Keychain factories are responsible for
  * creating personal keychains in Oslonøkkelen. Different factories can
  * create keychains granting users access to different doors.
- *
- * Todo: Turn into value class when Kotlin 1.5 is stable
  */
 data class KeychainFactoryId(val value: String) {
     init {
--- oslonokkelen-keychain-push-client/src/main/kotlin/com/github/oslokommune/oslonokkelen/kpc/model/KeychainFactoryInfo.kt
+++ oslonokkelen-keychain-push-client/src/main/kotlin/com/github/oslokommune/oslonokkelen/kpc/model/KeychainFactoryInfo.kt
@@ -2,7 +2,7 @@ package com.github.oslokommune.oslonokkelen.kpc.model
 
 import java.time.ZoneId
 
-data class KeychainFactoryInfo(val timezone: ZoneId, val id: KeychainFactoryId) {
-
-
-}
+data class KeychainFactoryInfo(
+    val timezone: ZoneId,
+    val id: KeychainFactoryId
+)
