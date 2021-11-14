801f08109931e1c29f89210596fe660ecb6e08d9
Kim A. Betti <kim.betti@gmail.com>
2021-11-14 17:02:07 +0100

    Cli - Deleted unused class

--- oslonokkelen-keychain-push-cli/src/main/kotlin/com/github/oslokommune/oslonokkelen/kpc/model/cli/Context.kt
+++
@@ -1,9 +0,0 @@
-package com.github.oslokommune.oslonokkelen.kpc.model.cli
-
-import com.github.oslokommune.oslonokkelen.kpc.ktor.OslonokkelenKeychainPushKtorClient
-import com.github.oslokommune.oslonokkelen.kpc.model.KeychainFactoryId
-
-class Context(
-    val keychainFactoryId: KeychainFactoryId,
-    val pushClient: OslonokkelenKeychainPushKtorClient
-)
