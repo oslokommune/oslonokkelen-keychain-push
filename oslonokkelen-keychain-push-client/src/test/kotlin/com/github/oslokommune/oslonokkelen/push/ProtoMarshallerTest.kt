84699a0ce89c28ef853998ffea0ef6ef6e367c04
Kim A. Betti <kim.betti@origo.oslo.kommune.no>
2022-10-11 10:48:35 +0200

    Push client

--- oslonokkelen-keychain-push-client/src/test/kotlin/com/github/oslokommune/oslonokkelen/push/ProtoMarshallerTest.kt
+++ oslonokkelen-keychain-push-client/src/test/kotlin/com/github/oslokommune/oslonokkelen/push/ProtoMarshallerTest.kt
@@ -7,15 +7,19 @@ import java.net.URI
 internal class ProtoMarshallerTest {
 
     @Test
-    fun `Can re-create`() {
-        val request = PushRequest.build("test") {
+    fun `Can re-create requests`() {
+        val request = PushRequest.build("booking-123") {
             addRecipientByPhoneNumber("47", "12345789")
             externalLink("More information", URI.create("https://vg.no"))
             additionalPlainTextInformation("Enjoy!")
 
             addPermission(
                 interval = TimeInterval.parse("2022-01-01", "12:00", "2022-01-05", "13:15"),
-                assetIds = listOf("x", "y")
+                assetIds = listOf("front-door", "back-door")
+            )
+            addPermission(
+                interval = TimeInterval.parse("2022-01-02", "12:00", "2022-01-05", "13:15"),
+                assetIds = listOf("kitchen")
             )
         }
 
