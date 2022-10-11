35bad589348b78712d9becb4677b11aa495c8162
Kim A. Betti <kim.betti@origo.oslo.kommune.no>
2022-10-11 10:49:53 +0200

    Test

--- oslonokkelen-keychain-push-client/src/test/kotlin/com/github/oslokommune/oslonokkelen/push/ProtoMarshallerTest.kt
+++ oslonokkelen-keychain-push-client/src/test/kotlin/com/github/oslokommune/oslonokkelen/push/ProtoMarshallerTest.kt
@@ -10,13 +10,30 @@ internal class ProtoMarshallerTest {
     fun `Can re-create requests`() {
         val request = PushRequest.build("booking-123") {
             addRecipientByPhoneNumber("47", "12345789")
-            externalLink("More information", URI.create("https://vg.no"))
-            additionalPlainTextInformation("Enjoy!")
+            externalLink("More information", URI.create("https://vg.no")) // Optional
+            additionalPlainTextInformation("Tørk av deg på beina før du går inn!") // Optional
 
             addPermission(
                 interval = TimeInterval.parse("2022-01-01", "12:00", "2022-01-05", "13:15"),
                 assetIds = listOf("front-door", "back-door")
             )
+            addPermission(
+                interval = TimeInterval.parse("2022-01-02", "12:00", "2022-01-05", "13:15"),
+                assetIds = listOf("kitchen")
+            )
+        }
+
+        val protobuf = ProtoMarshaller.toProtobuf(request)
+        val recreatedRequest = ProtoMarshaller.fromProtobuf(protobuf)
+
+        assertEquals(request, recreatedRequest)
+    }
+
+    @Test
+    fun `Can re-create request without link and additional information`() {
+        val request = PushRequest.build("booking-123") {
+            addRecipientByPhoneNumber("47", "12345789")
+
             addPermission(
                 interval = TimeInterval.parse("2022-01-02", "12:00", "2022-01-05", "13:15"),
                 assetIds = listOf("kitchen")
