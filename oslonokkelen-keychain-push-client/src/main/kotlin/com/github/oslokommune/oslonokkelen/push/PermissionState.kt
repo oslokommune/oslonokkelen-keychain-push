596fb580922f8c4f58b5faa1af840ce946a4881e
Lily Espirito Santo <lily.santo@dig.oslo.kommune.no>
2026-08-18 14:56:28 +0200

    Improvement: Return key code and name for recipients

--- oslonokkelen-keychain-push-client/src/main/kotlin/com/github/oslokommune/oslonokkelen/push/PermissionState.kt
+++ oslonokkelen-keychain-push-client/src/main/kotlin/com/github/oslokommune/oslonokkelen/push/PermissionState.kt
@@ -17,18 +17,32 @@ data class PermissionState(
     val information: Information?
 ) {
 
+    /**
+     * @param phoneNumber Phone number the key code was sent to
+     * @param canShare Whether the user can share this permission in the app
+     * @param keyCode The key code to be used in the app that was sent to the phone number
+     */
     data class PendingRecipient(
         val phoneNumber: PhoneNumber,
         val pushedAt: Instant,
-        val canShare: Boolean
+        val canShare: Boolean,
+        val keyCode: String
     )
 
+    /**
+     * @param phoneNumber Phone number the key code was sent to
+     * @param canShare Whether the user can share this permission in the app
+     * @param keyCode The key code that was used in the app by the user
+     * @param fullName Full name of the user that used the key code
+     */
     data class ConfirmedRecipient(
         val phoneNumber: PhoneNumber,
         val usageCounter: Int,
         val pushedAt: Instant,
         val confirmedAt: Instant,
-        val canShare: Boolean
+        val canShare: Boolean,
+        val keyCode: String,
+        val fullName: String
     )
 
 }
